a2051ec9759c2af6961ab014c305b17fa7687728
rockysmat <mraganyi@gmail.com>
2021-07-10 09:48:05 +0300

    first commit

--- SpeedConverter/src/SpeedConverter.java
+++ SpeedConverter/src/SpeedConverter.java
@@ -1,8 +1,17 @@
 public class SpeedConverter {
     private static final double kmPerHourToMilesPerHourRate  = 0.621371d;
     public static void main(String[] args) {
-        double kmPerHour = 10.25d;
+        double kmPerHour = -7d;
         System.out.println(kmPerHour + "KM/H to Miles/Hour =" +toMilesPerHour(kmPerHour));
+        printConversion(kmPerHour);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour >= 0){
+            System.out.println(kilometersPerHour +" km/h = " +toMilesPerHour(kilometersPerHour)+ " mi/h");
+        }
+        else
+            System.out.println("Invalid Value");
     }
 
     public static long toMilesPerHour(double kilometersPerHour){
